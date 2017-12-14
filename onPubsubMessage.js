d23f977f7e446834908048f509176b3fd4eeaba5
Rowan Smith <r.smith@internode.on.net>
2017-12-14 21:47:54 +1100

    update and rename

--- onPubsubMessage.js
+++ onPubsubMessage.js
@@ -7,12 +7,38 @@ const client = PubSub();
 
 const Buffer = require('safe-buffer').Buffer;
 
-exports.getMessage = function (event, callback) {
+exports.onPubsubMessage = function (event, callback) {
+  // event
+  // event.eventId
+  // event.timestamp
+  // event.eventType
+  // event.resource
+  // event.data
+  // event.data.data
+  // event.data.attributes
+  // event.data.attributes.key
+  // event.data.messageId
+  // event.data.publishTime
+  //
+  
+  // string. Unique ID for event
+  const eventId = event.eventId
+  
+  // string. iso 8601 datetime event created
+  const timestamp = event.timestamp
+  
+  // string. the type of event
+  const eventType = event.eventType
+  
+  // string. what made this event
+  const resource = event.resource
+  
+  // For a Pubsub message:
   // The payload is a message object in event.data
   const pubsubMessage = event.data;
-
+  
+  // In base64 encoding
   const data = Buffer.from(pubsubMessage.data, 'base64').toString()
-
 
   const attributes = pubsubMessage.attributes;
   // object. {'key0':'value0', 'key1':'value1' }
