a8d06736a327ef0f417245c20f4a928d03b90963
Rowan Smith <r.smith@internode.on.net>
2017-12-16 09:23:47 +1100

    Update index.js

--- gcf/pubsub/dataToDatastore/index.js
+++ gcf/pubsub/dataToDatastore/index.js
@@ -1,25 +1,37 @@
 'use strict';
-
+var datastore = require('@google-cloud/datastore')();
+const datastore = Datastore();
 const Buffer = require('safe-buffer').Buffer;
 
-exports.onPubsubMessage = function (event, callback) {
+exports.storeData = function storeData (event, callback) {
 
   // For a Pubsub message:
   // The payload is a message object in event.data
   const pubsubMessage = event.data;
   
   // In base64 encoding
-  const data = Buffer.from(pubsubMessage.data, 'base64').toString()
+  const data = JSON.parse(Buffer.from(pubsubMessage.data, 'base64').toString())
 
   // Debugging logs
   console.log(`
     data: ${data},
-    attributes: ${attributes},
-    messageId: ${messageId},
-    publishTime: ${publishTime}
     `);
+  
+  const messageId = event.data.messageId;
+  
+  // Datastore
+  var key = datastore.key(['datraFromPubsub', 'messageId']);
 
-  // TODO: add data to datastore
-
+  var entity = {
+    key: key,
+    data: data
+  };
+  
+  datastore.save(entity, function(err) {
+    if (!err) {
+    console.log('Message saved');
+    }
+  });
+  
   callback();
 };
