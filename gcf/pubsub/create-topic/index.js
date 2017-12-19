e576092f302415efe5c932d1d51047824d365783
Rowan Smith <r.smith@internode.on.net>
2017-12-20 10:46:38 +1100

    update functions

--- gcf/pubsub/create-topic/index.js
+++ gcf/pubsub/create-topic/index.js
@@ -12,16 +12,22 @@ const Buffer = require('safe-buffer').Buffer;
  * based on req the event data
  *
  * @param <String> event.data.data.topicName
+ *
+ * to use, publish {"topicName": "hello"}
+ * to subscribed topic
  */
 
-exports.CreateTopic = function (event, callback) {
+exports.createTopic = function createTopic(event, callback) {
   const pubsubMessage = event.data;
-  const data = Buffer.from(pubsubMessage.data, 'base64').toString()
-  const topicName = data.topicName
+  const data = Buffer.from(pubsubMessage.data, 'base64').toString();
+  console.log(`data: ${data}`);
 
+
+  const topicName = data.topicName;
+  console.log(`Topic Name: ${topicName}`);
   // Get client object
   const client = PubSub();
-  
+
   // Create topic,
   client.createTopic(topicName)
     .then((results) => {
@@ -29,7 +35,6 @@ exports.CreateTopic = function (event, callback) {
       // Output to console
       console.log(`Created new topic: ${topic.name}`);
     });
-}
 
   callback();
 };
